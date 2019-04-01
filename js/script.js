261deebbaa1771e6fdd3483e5c131762b26612a6
Alexei Rodriguez <alexei.rodriguez95@hotmail.com>
2019-03-31 22:05:31 -0600

    now when you scroll nav gets hidden

--- js/script.js
+++ js/script.js
@@ -16,3 +16,19 @@ function showHideNav() {
         chevron.classList.remove('header__toggle__chevron--original-position');
     }
   }
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+    document.getElementById("header").style.boxShadow = "0px 15px 35px rgba(1, 1, 1,0.5)";
+    document.getElementById("logo").style.boxShadow = " 4px 4px 25px hsla(232, 100%, 50%,0.5)";
+    } else {
+      document.getElementById("header").style.top = "-50px";
+      document.getElementById("header").style.boxShadow = "none";
+      document.getElementById("logo").style.boxShadow = "none";
+      document.getElementById("nav").classList.add("nav--hide");
+    }
+    prevScrollpos = currentScrollPos;
+  }
